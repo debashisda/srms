b261fd2553b85fcaed9fd4db9e34f1694ddb3740
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-04-19 13:41:18 +0530

    Update index.php

--- srms/index.php
+++ srms/index.php
@@ -30,7 +30,7 @@ if(isset($login))
     <meta content="text/html; charset=UTF-8">    
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">  
-    <link href="./login_files/signin.css" rel="stylesheet">     
+    <link href="./css/signin.css" rel="stylesheet">     
 </head>
 <body class="text-center">
     <form class="form-signin" method="post">
