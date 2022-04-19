9f64aecd4c0291a867072e83d42a8756d99755cb
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-04-19 13:33:42 +0530

    Update index.php

--- srms/index.php
+++ srms/index.php
@@ -1,86 +1,48 @@
 <?php
-
-$msg = false;
-include('common/super_common.php');
-
 extract($_POST);
 if(isset($login))
 {
-  /*
-  $username=$username;
-	$password=hash('sha256',$password);
-  */
-    $query=mysqli_query($con,"select * from stu_details where email='".$username."' && password='".$password."';");
-	$ret=mysqli_fetch_assoc($query);
-	if($ret>0)
-	{
-        session_start();
-        $_SESSION['roll'] = $ret['roll'];
-        $_SESSION['regno'] = $ret['regno'];
-        $_SESSION['course'] = $ret['course'];
-        $_SESSION['email'] = $ret['email'];
-        $_SESSION['name'] = $ret['name'];
-        $_SESSION['state'] = true;
-		header('location:student/student_dashboard.php');
-	}
-   else
-	{
-		$msg=true;
-	}
+  include('common/super_common.php'); 
+  $ret=mysqli_fetch_assoc(mysqli_query($con,"select * from stu_details where email='".$username."' && password='".$password."';"));
+  mysqli_close($con);
+  if($ret>0)
+  {
+      session_start();
+      $_SESSION['roll'] = $ret['roll'];
+      $_SESSION['course'] = $ret['course'];
+      $_SESSION['email'] = $ret['email'];
+      $_SESSION['name'] = $ret['name'];
+      $_SESSION['state'] = true;
+      header('location:student/dashboard.php');
+  }
+  else
+  {
+    $msg="<div class='alert alert-danger alert-dismissible' role='alert'>Invalid Username or Password
+            <button class='close' data-dismiss='alert'>&times;</button>
+          </div>";
+  }
 }
 ?>
-<html>
-<head>		
-    <link rel="stylesheet" href="common/indexstyle.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SRMS</title>
+    <meta content="text/html; charset=UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">  
+    <link href="./login_files/signin.css" rel="stylesheet">     
 </head>
-<style>
-	
-@font-face 
-{
-	font-family: bootstrap-icons;
-    src: url(common/svg/bootstrap-icons.woff2);
-}
-
-[class*=" bi-"]::before {
-  display: inline-block;
-  font-family: bootstrap-icons;
-  font-style: normal;
-  font-weight: normal;
-  font-variant: normal;
-  text-transform: none;
-  line-height: 1;
-  vertical-align: text-bottom;
-  -webkit-font-smoothing: antialiased;
-}
-.bi-eye-slash::before { content: "\f320"; }
-.bi-eye::before { content: "\f321"; }
-
-	</style>
-    <body>
-        <h1>Student Result Management System</h1>
-        <?php if($msg) echo "<ul class='auth-form' id='clsem'><li>Invalid username or password.<span class='close'  onclick='errormessage()'>&times;</span></li></ul>";?>
-
-        <div class="auth-form" id="login">
-            <div class="auth-form-body">
-                <form method="post">
-                    <label>Email:</label>
-                    <input type="email" name="username" id="login_field" class="form-control" required autocomplete>
-                    <div class="position-relative">
-                        <label>Password:</label>
-                        <input type="password" name="password" id="password" class="form-control" required>
-                        <i class="bi bi-eye-slash eye-button" id="togglePassword"></i>
-                        <input type="submit" class="btn" name="login" value="Log in">
-                        <a class="label-link" href="forgot-password.php">Forgot password?</a>
-                    </div>
-                </form>
-            </div>
-            <!--p class="create-new-account">New here? <a href="create-account.php">Create an account</a>.</p-->
-        </div>
-        <script>
-            const togglePassword = document.querySelector('#togglePassword');
-            const password = document.querySelector('#password');
-            togglePassword.addEventListener('click', function(e){const type = password.getAttribute('type')==='password'?'text':'password';password.setAttribute('type',type);this.classList.toggle('bi-eye');});
-		</script>
-		<script>function errormessage(){document.getElementById("clsem").style.display='none';}</script>
-    </body>
-    </html>
+<body class="text-center">
+    <form class="form-signin" method="post">
+        <?php if(isset($msg)){ echo $msg; } ?>
+        <!--img class="mb-4" src="./login_files/bootstrap-solid.svg" alt="" width="72" height="72"-->
+        <h1 class="h3 mb-3 font-weight-normal">Welcome back</h1>      
+        <input type="email" class="form-control" name="username" placeholder="Username" required>      
+        <input type="password" class="form-control" name="password" placeholder="Password" required>
+        <span> Forgot Password? <a href="common/forgot_password.php" class="link-primary">Click here.</a></span>
+        <button class="btn btn-md btn-primary btn-block" name="login" type="submit">Sign in</button>      
+    </form> 
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
