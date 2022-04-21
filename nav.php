60a6b7f0a7f96d6771ec980c1969a598c9565596
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-04-22 03:34:36 +0530

    Delete nav.php

--- nav.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar navbar-light bg-light">
-	<a class="navbar-brand"><img src="../css/bootstrap-solid.svg" height="30" class="d-inline-block align-top"> SRMS</a>
-	<div class="my-sm-0">
-		<a href="dashboard.php" id="dashboard"><button type="button" class="btn btn-secondary btn-sm">Dashboard</button></a>
-		<button type="button" class="btn btn-info btn-sm" id="print" onclick="window.print()">Print Result</button>
-		<a href="../logout.php"><button type="button" class="btn btn-danger btn-sm">Logout</button></a>
-	</div>		
-</nav>
