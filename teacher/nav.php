0fba3b54cb201321a43dc9700c61c68f3f763c69
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-04-22 13:07:14 +0530

    Delete teacher directory

--- teacher/nav.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar navbar-light bg-light">
-	<a class="navbar-brand"><img src="../css/bootstrap-solid.svg" height="30" class="d-inline-block align-top"> SRMS</a>
-	<div class="my-sm-0">
-		<a href="" id="back"><button type="button" class="btn btn-secondary btn-sm">Back</button></a>
-		<a href="../logout.php"><button type="button" class="btn btn-danger btn-sm">Logout</button></a>
-	</div>		
-</nav>
