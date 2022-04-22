74a3613ec03388038e3c9b46a19c618868d5e905
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-04-22 12:40:56 +0530

    Update upload.php

--- teacher/upload.php
+++ teacher/upload.php
@@ -12,13 +12,27 @@
 	{		
 		$res=mysqli_query($con,"select sub_code from subjects where course='bca' and sem=".$sem);
 		$Q="";
+		$t=0;
+		$k=0;
 		while($r=mysqli_fetch_assoc($res))
 		{   
 		    $x=$r['sub_code'];
-		    $Q = $Q.$x.":".${$x.'A'}.":".${$x.'B'}."|";  	
+		    $t++;
+		    $Q = $Q.$x.":".${$x.'A'}.":".${$x.'B'}."|";
+		    if(${$x.'A'} == NULL || ${$x.'B'} == NULL)
+		    {
+			    $k++;
+		    }   	
 		}
 		$Q = trim($Q,'|');
-		$query = "update bca set sem".$sem."='".$Q."' where roll =".$roll;
+		if($t == $k)		
+		{
+			$query = "update bca set sem".$sem."=NULL where roll=".$roll;
+		}	
+		else		
+		{
+			$query = "update bca set sem".$sem."='".$Q."' where roll =".$roll;
+		}				
 		mysqli_query($con,$query);
 		mysqli_close($con);
 		header("location:moredetails.php?roll=".$roll);		
