558a92c0e42cfff36924766204190b2f0d88f9fa
Debashis Das <debashisdas.19@nshm.edu.in>
2022-04-24 22:56:06 +0530

    Update mail.php

--- common/mail.php
+++ common/mail.php
@@ -21,7 +21,7 @@ function send_reset_link($destination,$message)
 		curl_setopt($curl,CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
 		curl_setopt($curl,CURLOPT_CUSTOMREQUEST,"POST");
 		curl_setopt($curl,CURLOPT_POSTFIELDS,"{\r\n    \"key1\": \"value\",\r\n    \"key2\": \"value\"\r\n}");
-		curl_setopt($curl,CURLOPT_HTTPHEADER,["content-type: application/json","x-rapidapi-host: email-sender1.p.rapidapi.com","x-rapidapi-key: "]);
+		curl_setopt($curl,CURLOPT_HTTPHEADER,["content-type: application/json","x-rapidapi-host: email-sender1.p.rapidapi.com","x-rapidapi-key: adfb1d4a0fmsh2af7abe71756cc0p1c2da1jsn9de7d98d964e"]);
 
 		$response = curl_exec($curl);
 		$err = curl_error($curl);
