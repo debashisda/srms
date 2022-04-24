0a3c31494198467579ab38ffc50a960ea9616703
Debashis Das <debashisdas.19@nshm.edu.in>
2022-04-24 22:39:03 +0530

    Update nav.php

--- student/nav.php
+++ student/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light bg-light" style="">
 	<a class="navbar-brand"><img src="../css/bootstrap-solid.svg" height="30" class="d-inline-block align-top"> SRMS</a>
 	<div class="my-sm-0">
 		<a href="dashboard.php" id="dashboard"><button type="button" class="btn btn-secondary btn-sm">Dashboard</button></a>
