a360c888315e958300e4ee11db194745afcdb74c
Debashis Das <debashisdas.19@nshm.edu.in>
2022-04-27 01:11:37 +0530

    bug fixing

--- student/nav.php
+++ student/nav.php
@@ -1,10 +1,10 @@
 <?php
-<div class="navbar navbar-light bg-light" style="">
-	<a class="navbar-brand"><img src="../css/bootstrap-solid.svg" height="30" class="d-inline-block align-top"> SRMS</a>
-	<div class="my-sm-0">
-		<a href="dashboard.php" id="dashboard"><button type="button" class="btn btn-secondary btn-sm">Dashboard</button></a>
-		<button type="button" class="btn btn-info btn-sm" id="print" onclick="window.print()">Print Result</button>
-		<a href="../logout.php"><button type="button" class="btn btn-danger btn-sm">Logout</button></a>
+echo "<div class='navbar navbar-light bg-light'>
+	<a class='navbar-brand'><img src='../css/bootstrap-solid.svg' height='30' class='d-inline-block align-top'> SRMS</a>
+	<div class='my-sm-0'>
+		<a href='dashboard.php' id='dashboard'><button type='button' class='btn btn-secondary btn-sm'>Dashboard</button></a>
+		<button type='button' class='btn btn-info btn-sm' id='print' onclick='window.print()'>Print Result</button>
+		<a href='../logout.php'><button type='button' class='btn btn-danger btn-sm'>Logout</button></a>
 	</div>		
-</div>
+</div>";
 ?>
--- teacher/nav.php
+++ teacher/nav.php
@@ -1,9 +1,9 @@
 <?php
-<div class="navbar navbar-light bg-light" style="margin-bottom:20px;background-color:#cfe1f2 !important;">
-	<a class="navbar-brand"><img src="../css/bootstrap-solid.svg" height="30" class="d-inline-block align-top"> SRMS</a>
-	<div class="my-sm-0">
-		<a href="" id="back"><button type="button" class="btn btn-secondary btn-sm">Back</button></a>
-		<a href="../logout.php"><button type="button" class="btn btn-danger btn-sm">Logout</button></a>
+echo "<div class='navbar navbar-light bg-light' style='margin-bottom:20px;background-color:#cfe1f2 !important;'>
+	<a class='navbar-brand'><img src='../css/bootstrap-solid.svg' height='30' class='d-inline-block align-top'> SRMS</a>
+	<div class='my-sm-0'>
+		<a id='back'><button type='button' class='btn btn-secondary btn-sm'>Back</button></a>
+		<a href='../logout.php'><button type='button' class='btn btn-danger btn-sm'>Logout</button></a>
 	</div>		
-</div>
+</div>";
 ?>
