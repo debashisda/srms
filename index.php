ce2c08fa81091128e59b39ab6c9f3c5d8b0a0972
Debashis Das <debashisdas.19@nshm.edu.in>
2022-05-07 23:26:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//b587d6ea53msh139614862a5f515p1712e7jsn1298da1d2ef2
 error_reporting(0);
 session_start();
 if ($_SESSION['state'])  header('location:student/dashboard.php');
@@ -8,43 +9,43 @@ if ($_SESSION['state2']) header('location:admin/index.php');
 extract($_POST);
 if(isset($login))
 {
-  include('common/super_common.php'); 
-  $stu=mysqli_fetch_assoc(mysqli_query($con,"select * from stu_details where email='".$username."' and password='".$password."';"));  
-  $tch=mysqli_fetch_assoc(mysqli_query($con,"select * from tch_details where email='".$username."' and password='".$password."';")); 
-  $adm=mysqli_fetch_assoc(mysqli_query($con,"select * from adm_details where email='".$username."' and password='".$password."';"));
-  mysqli_close($con);  
-  if($stu>0)
-  {      
-      $_SESSION['roll'] = $stu['roll'];
-      $_SESSION['course'] = $stu['course'];
-      $_SESSION['email'] = $stu['email'];
-      $_SESSION['name'] = $stu['name'];
-      $_SESSION['state'] = true;
-      header('location:student/dashboard.php');
-  } 
-  elseif($tch>0)
-  {
-      $_SESSION['id'] = $tch['id'];
-      $_SESSION['name'] = $tch['name'];
-      $_SESSION['email'] = $tch['email'];
-      $_SESSION['ca'] = $tch['ca'];
-      $_SESSION['state1'] = true;
-      header('location:teacher/dashboard.php');
-  }  
-  elseif($adm>0)
-  {
-      $_SESSION['id'] = $adm['id'];
-      $_SESSION['name'] = $adm['name'];
-      $_SESSION['email'] = $adm['email'];
-      $_SESSION['state2'] = true;
-      header('location:admin/index.php');
-  }
-  else
-  {
-      $msg="<div class='alert alert-danger alert-dismissible' role='alert'>
-              Invalid Username or Password<button class='close' data-dismiss='alert'>&times;</button>
-            </div>";
-  }
+    include('common/super_common.php'); 
+    $stu=mysqli_fetch_assoc(mysqli_query($con,"select * from stu_details where email='".$username."' and password='".$password."';"));  
+    $tch=mysqli_fetch_assoc(mysqli_query($con,"select * from tch_details where email='".$username."' and password='".$password."';")); 
+    $adm=mysqli_fetch_assoc(mysqli_query($con,"select * from adm_details where email='".$username."' and password='".$password."';"));
+    mysqli_close($con);  
+    if($stu>0)
+    {      
+        $_SESSION['roll'] = $stu['roll'];
+        $_SESSION['course'] = $stu['course'];
+        $_SESSION['email'] = $stu['email'];
+        $_SESSION['name'] = $stu['name'];
+        $_SESSION['state'] = true;
+        header('location:student/dashboard.php');
+    } 
+    elseif($tch>0)
+    {
+        $_SESSION['id'] = $tch['id'];
+        $_SESSION['name'] = $tch['name'];
+        $_SESSION['email'] = $tch['email'];
+        $_SESSION['ca'] = $tch['ca'];
+        $_SESSION['state1'] = true;
+        header('location:teacher/dashboard.php');
+    }  
+    elseif($adm>0)
+    {
+        $_SESSION['id'] = $adm['id'];
+        $_SESSION['name'] = $adm['name'];
+        $_SESSION['email'] = $adm['email'];
+        $_SESSION['state2'] = true;
+        header('location:admin/index.php');
+    }
+    else
+    {
+        $msg="<div class='alert alert-danger alert-dismissible' role='alert'>
+                Invalid Username or Password<button class='close' data-dismiss='alert'>&times;</button>
+              </div>";
+    }
 
 }
 
