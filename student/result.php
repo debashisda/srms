96e1757f2b0037b433e7d34c7c4d1f1fa9b8554d
Debashis Das <debashisdas.19@nshm.edu.in>
2022-05-07 23:25:46 +0530

    Update result.php

--- student/result.php
+++ student/result.php
@@ -3,18 +3,20 @@ error_reporting(0);
 session_start();
 if(!($_SESSION['state'])) header('location:../logout.php');
 $sem = $_GET['sem'];
-
 if(!isset($sem) || ($sem<=0 || $sem > $_SESSION['RESULT_COUNT'])) header('location:dashboard.php');
 
 function grade_to_marks($al_m)
 {
-	if($al_m == 'O') return 10;
-	elseif ($al_m == 'E') return 9;
-	elseif ($al_m == 'A') return 8;		
-	elseif ($al_m == 'B') return 7;
-	elseif ($al_m == 'C') return 6;
-	elseif ($al_m == 'D') return 5;
-	else return 4;		
+	switch ($al_m) 
+	{	
+		case 'O' : return 10;
+		case 'E' : return 9;
+		case 'A' : return 8;
+		case 'B' : return 7;
+		case 'C' : return 6;
+		case 'D' : return 5;
+		default  : return 4;
+	}		
 }
 
 ?>
@@ -23,7 +25,7 @@ function grade_to_marks($al_m)
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Result</title>
+	<title><?php echo $_SESSION['roll']." Sem ".$sem; ?></title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/student.css">
 </head>
@@ -41,22 +43,27 @@ function grade_to_marks($al_m)
 		    <table class="table table-bordered table-striped table-hover table-condensed">
 		    	<thead><tr><th>Subject Code</th><th>Subjects Offered</th><th>Letter Grade</th><th>Points</th><th>Credit</th><th>Credit Points</th></tr></thead>
 				<tbody>
-				<?php				
-				$query = "SELECT sem".$sem." FROM ".$_SESSION['course']." WHERE roll=".$_SESSION['roll'];
+				<?php		
 				include_once("../common/super_common.php");
-				$st = mysqli_fetch_assoc(mysqli_query($con,$query));		
+				$st = mysqli_fetch_assoc(mysqli_query($con,"SELECT sem".$sem." FROM ".$_SESSION['course']." WHERE roll=".$_SESSION['roll']))['sem'.$sem];	
 				$c = 0;	$cp = 0;
-				$a = explode('|',$st['sem'.$sem]);		
-				$p = False;
-				for($i=0;$i<count($a);$i++)
+				$a = explode('|',$st);				
+				for($i=0; $i<count($a); $i++)
 				{		
 					$b=explode(':',$a[$i]);
-					if($b[2] == NULL || $b[1] == NULL)	$p=True;
+					if($b[2] == NULL or $b[1] == NULL) continue;
 					else
 					{					
-						$sn = mysqli_fetch_assoc(mysqli_query($con,"select sub_name from subjects where sub_code='".$b[0]."'"));
+						$sn = mysqli_fetch_assoc(mysqli_query($con,"select sub_name from subjects where sub_code='".$b[0]."'"))['sub_name'];
 						$nm = grade_to_marks($b[1]);
-						echo "<tr><td>".$b[0]."</td><td class='sub-n'>".$sn['sub_name']."</td><td>".$b[1]."</td><td>".$nm."</td><td>".$b[2]."</td><td>".$nm*$b[2]."</td></tr>";
+						echo "<tr>
+								<td>".$b[0]."</td>
+								<td class='sub-n'>".$sn."</td>
+								<td>".$b[1]."</td>
+								<td>".$nm."</td>
+								<td>".$b[2]."</td>
+								<td>".$nm*$b[2]."</td>
+							  </tr>";
 						$c+=$b[2];
 						$cp+=($nm*$b[2]);
 					}
@@ -70,7 +77,7 @@ function grade_to_marks($al_m)
 			<table class="table table-bordered table-striped table-hover table-condensed">
 				<tbody>
 					<tr><th class="u-data" colspan='2'>RESULT</th></tr>
-		    		<tr><th class="u-data wide">SGPA</th><th class="u-data"><?php echo $sgpa;  if($p){ echo " <i>*Partial Result</i>";}?></th></tr>
+		    		<tr><th class="u-data wide">SGPA</th><th class="u-data"><?php echo $sgpa; ?></th></tr>
 					<tr><th class="u-data">Result</th><th class="u-data"><?php echo ($sgpa>=5.6)?"Pass":"Fail"; ?></th></tr>
 				</tbody>				
 			</table>
