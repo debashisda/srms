e99be332bd0d4cd3fcfcba2e93c0a25524113c53
Debashis Das <debashisdas.19@nshm.edu.in>
2022-06-04 18:23:11 +0530

    Database error fixed while setting NULL value

--- teacher/result.php
+++ teacher/result.php
@@ -25,8 +25,8 @@
 		}
 		$Q = trim($Q,'|');		
 		$c = mysqli_fetch_assoc(mysqli_query($con,"select count(sub_code) as sub from subjects where sem=".$sem." and course='".$_SESSION['ca']."'"));	
-		if($NULLR == $c['sub'])	$Q = NULL;			
-		mysqli_query($con,"update ".$_SESSION['ca']." set sem".$sem."='".$Q."' where roll =".$roll);	
+		if($NULLR == $c['sub'])	mysqli_query($con,"update ".$_SESSION['ca']." set sem".$sem."=NULL where roll =".$roll);
+		else mysqli_query($con,"update ".$_SESSION['ca']." set sem".$sem."='".$Q."' where roll =".$roll);	
 		$msg = true;		
 	}
 ?>
@@ -36,54 +36,64 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 	<link rel="stylesheet" type="text/css" href="../css/teacher.css">
+	<link rel="stylesheet" type="text/css" href="../css/admin.css">
 	<title>SRMS</title>		
 </head>
 <body class="d-flex flex-column h-100">
-		<header>
-		<div class="d-flex flex-column flex-md-row align-items-center p-2 px-md-4 mb-3 bg-light border-bottom">
-			<h5 class="my-0 mr-md-auto font-weight-normal">SRMS</h5>
-			<nav class="my-2 my-md-0">   
-				<a href="dashboard.php" class="btn btn-secondary btn-sm">Dashboard</a>		    			
-				<a href="account.php" class="btn btn-primary btn-sm">Account</a>
-				<a href="../logout.php" class="btn btn-danger btn-sm">Logout</a>
-			</nav>  
+	<?php include_once("../common/topbar.php");?>
+	<div class="container-fluid">
+        <div class="row">
+			<?php include_once("sidebar.php");?>	
+			<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+				<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                  	<h1 class="h2">Result</h1>
+                  	<div class="btn-toolbar mb-2 mb-md-0">
+			          <div class="btn-group me-2">
+			            <a id="back" ><button type="button" class="btn btn-sm btn-outline-secondary">Back</button></a>	           
+			          </div>          
+        			</div>                  	  
+               	</div>
+				<div class="row">	
+					<div class="container-fluid">				
+						<?php if($msg) echo "<div class='alert alert-success alert-dismissible' role='alert' style='text-align:left;'>Result Updated</div>"; ?>
+						<div class="table-responsive">
+							<table class="table table-bordered table-striped table-hover table-condensed">
+								<tbody>	
+								<style type="text/css">.u-data{width: 25%;}.bg-light{background:#cfe1f2 ;}</style>				
+									<tr>
+										<th class="u-data">NAME: <?php echo $nm; ?></th>
+										<th class="u-data">ROLL NO: <?php echo $_GET['roll']; ?></th>		
+										<th class="u-data">COURSE: <?php echo strtoupper($_SESSION['ca']); ?></th>
+										<th class="u-data">SEM: <?php echo strtoupper($_GET['sem']); ?></th>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="table-responsive">
+							<form method="post">
+								<table class="table table-bordered table-striped table-hover table-condensed">
+									<thead class="thead-dark"><tr><th>Subject Code</th><th>Subjects Name</th><th>Letter Grade</th><th>Credit</th></tr></thead>
+									<tbody>  		
+									<?php	
+										include_once("../common/super_common.php");					
+										$row=mysqli_fetch_assoc(mysqli_query($con,"select sem".$sem." from ".$_SESSION['ca']." where roll=".$roll));	
+										if($row['sem'.$sem] !== NULL) include_once('update.php');
+										else if($row['sem'.$sem] == "")
+											include_once('insert.php');							
+									?>
+									</tbody>
+								</table>
+							</form>
+						</div>
+					</div>
+				</div>
+			</main>
 		</div>
-	</header>	
-	<div class="container">
-		<?php if($msg) echo "<div class='alert alert-success alert-dismissible' role='alert' style='text-align:left;'>Result Updated<button class='close' data-dismiss='alert'>&times;</button></div>"; ?>
-		<div class="table-responsive">
-			<table class="table table-bordered table-striped table-hover table-condensed">
-				<tbody>	
-				<style type="text/css">.u-data{width: 25%;}.bg-light{background:#cfe1f2 ;}</style>				
-					<tr>
-						<th class="u-data">NAME: <?php echo $nm; ?></th>
-						<th class="u-data">ROLL NO: <?php echo $_GET['roll']; ?></th>		
-						<th class="u-data">COURSE: <?php echo strtoupper($_SESSION['ca']); ?></th>
-						<th class="u-data">SEM: <?php echo strtoupper($_GET['sem']); ?></th>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<div class="table-responsive">
-			<form method="post">
-				<table class="table table-bordered table-striped table-hover table-condensed">
-					<thead class="thead-dark"><tr><th>Subject Code</th><th>Subjects Name</th><th>Letter Grade</th><th>Credit</th></tr></thead>
-					<tbody>  		
-					<?php	
-						include_once("../common/super_common.php");					
-						$row=mysqli_fetch_assoc(mysqli_query($con,"select sem".$sem." from ".$_SESSION['ca']." where roll=".$roll));	
-						if($row['sem'.$sem] !== NULL) include_once('update.php');
-						else include_once('insert.php');							
-					?>
-					</tbody>
-				</table>
-			</form>
-		</div>
-	</div>
+	</div>	
 	<?php include('../common/footer.php'); ?>
 	<script src="../js/trestrict.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="../js/backhref.js"></script>
 </body>
 </html>				
