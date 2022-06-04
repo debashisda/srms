a07ddd4a55a742be88cd9dafb63782261da202df
Debashis Das <debashisdas.19@nshm.edu.in>
2022-06-04 18:28:58 +0530

    Part of New Design

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,6 @@
 <footer class="footer mt-auto py-2 bg-light">
 	<div class="container"><center class="text-muted">SRMS</center></div>
 </footer>
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>
+<script type="text/javascript">window.setTimeout(function(){$(".alert").slideUp(200, function(){$(this).remove();});}, 2500);</script>
