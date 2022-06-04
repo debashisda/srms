388c6206f65cddadf6ea486851abc37f5c3acdb5
Debashis Das <debashisdas.19@nshm.edu.in>
2022-06-04 18:24:06 +0530

    Part of the New Design

---
+++ common/topbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="index.html"><strong>S R M S</strong></a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>  
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+            <a class="nav-link top" href="account.php"><?php echo $_SESSION['name']; ?> <i class="bi bi-person-circle"></i></a>
+        </li>
+    </ul>
+</nav>
