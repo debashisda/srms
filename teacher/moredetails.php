a329e02bda84dea6bbbf79707d820fb1a3ecd85e
Debashis Das <debashisdas.19@nshm.edu.in>
2022-06-04 18:32:09 +0530

    New Design

--- teacher/moredetails.php
+++ teacher/moredetails.php
@@ -15,22 +15,28 @@ mysqli_close($con);
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 	<link rel="stylesheet" type="text/css" href="../css/teacher.css">
+	<link rel="stylesheet" type="text/css" href="../css/admin.css">
 	<title>SRMS</title>
 </head>
 <body class="d-flex flex-column h-100">
-	<header>
-		<div class="d-flex flex-column flex-md-row align-items-center p-2 px-md-4 mb-3 bg-light border-bottom">
-			<h5 class="my-0 mr-md-auto font-weight-normal">SRMS</h5>
-			<nav class="my-2 my-md-0">   
-				<a href="dashboard.php" class="btn btn-secondary btn-sm">Dashboard</a>		    			
-				<a href="account.php" class="btn btn-primary btn-sm">Account</a>
-				<a href="../logout.php" class="btn btn-danger btn-sm">Logout</a>
-			</nav>  
-		</div>
-	</header>
-	<div class="container">	
+	<?php include_once("../common/topbar.php");?>
+	<div class="container-fluid">
+        <div class="row">
+			<?php include_once("sidebar.php");?>	
+			<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+				<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                  	<h1 class="h2">Dashboard</h1>
+                  	<div class="btn-toolbar mb-2 mb-md-0">
+			          <div class="btn-group me-2">
+			            <a id="back" ><button type="button" class="btn btn-sm btn-outline-secondary">Back</button></a>			           
+			          </div>          
+        			</div>                  	  
+               	</div>
+				<div class="row" style="padding-left: 15px;padding-right: 15px;">	
 		<div class="table-responsive">
+
 			<table class="table table-bordered table-striped table-hover table-condensed">
 				<tbody>					
 					<tr><th class="u-data">NAME: <?php echo $nm; ?></th><th class="u-data">ROLL NO: <?php echo $_GET['roll']; ?></th>		
@@ -63,8 +69,14 @@ mysqli_close($con);
 			</table>
 		</div>
 	</div>
-	<?php include('../common/footer.php'); ?>	
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</main>
+</div>
+</div>
+
+
+
+	<?php include('../common/footer.php'); ?>
+	<script src="../js/backhref.js"></script>
+	
 </body>
 </html>
